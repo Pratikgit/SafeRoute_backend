e5ca6f63b0e1d4154be6cd20e1e98d6ee0f5b289
vishalrathod05 <vishal.rathod05@gmail.com>
2016-12-01 23:51:32 -0500

    Changes for MongoDB Connection using PyMongo
    Made changes to connect MongoDB using PyMongo and extract the routes information and factors for safe score calculation

---
+++ route_app/route_app/model/safe_model.py
@@ -0,0 +1,57 @@
+import pymongo
+from pymongo import MongoClient
+
+class safe_model():
+    def __init__(self):
+        pass
+
+    def findPathDetailsByID(self,host,db,collection,search_id):
+        hconn = self.getConnection(host)
+        hdb = self.getDBDetails(hconn, db)
+        hcoll = self.getCollectionDetails(hdb, collection)
+
+        try:
+	    query = {"path_id" : search_id}
+            cursor = hcoll.find(query, {"crime_count":1, "accident_count":1, "hospital_police_services":1, "_id":0})
+        except Exception as e:
+            print "Unexpected error:", type(e), e
+        return cursor
+  
+    def findFactorWeightage(self,host,db,collection,search_id):
+        hconn = self.getConnection(host)
+        hdb = self.getDBDetails(hconn, db)
+        hcoll = self.getCollectionDetails(hdb, collection)
+
+        query = {"factor_name" : search_id}
+        try:
+            cursor = hcoll.find(query, {"weightage":1, "_id":0})
+        except Exception as e:
+            print "Unexpected error:", type(e), e
+        return cursor
+
+    def getConnection(self,host):
+        try:
+            retConnection = MongoClient(host)
+        except Exception  as e:
+            print 'Exception occurred while creating connection with mongoDB, value: \n '
+            print e
+        return retConnection
+
+    def getDBDetails(self,connection,db):
+        # create a database connection
+        try :
+            retDb = connection[db] # NOTE : dictionary style access
+        except Exception as e:
+            print 'Exception occurred while connecting to database %s, value: \n ' % db
+            print e
+        return retDb
+
+    def getCollectionDetails(self,db,collection):
+        #create a handle for collection
+        try :
+            retCollection = db[collection] # NOTE : dictionary style access
+        except Exception as e:
+            print 'Exception occurred while getting details for  collection:  %s, value: \n ' % collection
+            print e
+        return retCollection
+
