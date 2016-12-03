c5e86dbf34a1e283a25155637ce79571126e4dec
Mayur Pate <mayurpate91@gmail.com>
2016-12-03 18:36:23 -0500

    Complted Approximation Algorithm

--- route_app/route_app/controllers/safe_controller.py
+++ route_app/route_app/controllers/safe_controller.py
@@ -20,51 +20,97 @@ class SafeControllerController(BaseController):
     a = safe_model()
 
     def index(self):
-	path_count = 1
+        final_paths = []
         maps = self.init_google_maps('AIzaSyAF8n02MUVzzlMriqUMYb7rlSkaeBW1qDs')
         if not maps:
             log.info("Please Check Your API Key...")
-        log.info("Maps Intialized...") 
 
         source = request.params.get("source")
         destination = request.params.get("destination")
         all_routes = self.find_all_routes(maps, source, destination)
+        path_score_dict = self.fetchPathsInfoFromDB(all_routes)
+ 
+        route_distance_map = dict(zip(path_score_dict.get('path_array'), all_routes.get('distance_array')))
+        route_safe_map = dict(zip(path_score_dict.get('path_array'), path_score_dict.get('score_array')))
+        
+        log.info("Route Safe Map:%s" %route_safe_map)
+        log.info("Route Distance Map:%s" %route_distance_map)
+        
+        safe_path_list = self.make_list_of_safe_paths(route_distance_map, route_safe_map)
+        log.info("Safe Path List:%s" %safe_path_list)
 
-	for route in all_routes.get('routes'):
-	   pathScore = self.fetchPathsInfoFromDB(route)	
-           log.info("Safe Score of Path %s:%s" %(path_count, pathScore))
-	   path_count = path_count + 1
+        for search_id in safe_path_list:
+            index = path_score_dict.get('path_array').index(search_id)
+            final_paths.append(all_routes.get('routes')[index])
 
-        return json.dumps(all_routes)
+        log.info("Final Paths Polyline:%s" %final_paths)
+        return json.dumps({'routes': final_paths})
+
+    def make_list_of_safe_paths(self, route_distance_map, route_safe_map):
+        final_route_list = []
+
+        #Delete worst path
+        max_path_tuple = max(route_safe_map.items(), key = lambda x: x[1])
+        route_distance_map.pop(max_path_tuple[0])
+        route_safe_map.pop(max_path_tuple[0])
+        final_route_list.append(max_path_tuple[0])
+
+        #Compare and remove paths whoose safe score is double to any other score
+        for route,score in route_safe_map.iteritems():
+            for temp_score in route_safe_map.values():
+                if temp_score and score >= 2* temp_score:
+                    final_route_list.append(route)
+                    route_safe_map.pop(route)
+                    route_distance_map.pop(route)
+                    break
+
+        #if more than 1 remains then return paths based on Distance 
+        if len(route_safe_map.keys()) == 1:
+            final_route_list.append(route)
+        else:
+            sorted_distance = sorted(route_distance_map.items(), key=lambda x: x[1], reverse = True)
+            for path_tuple in sorted_distance:
+                final_route_list.append(path_tuple[0])
+ 
+        return final_route_list[::-1] 
 
     def init_google_maps(self, api_key):
         maps = googlemaps.Client(key = api_key)
         return maps if maps else None
 
-    def find_all_routes(self, g_maps, source, destination, mode = 'driving', need_all_routes = True):
+    def find_all_routes(self, g_maps, source, destination, mode = 'driving', need_all_routes = True): 
         polyline_string_list = []
+        distance_array = []
         all_routes = g_maps.directions(source, destination, mode = mode, 
                                 alternatives = need_all_routes)
+        log.info("Length of all routes:%s" %(len(all_routes)))
+
         for route in all_routes:
             polyline_string_list.append(route.get('overview_polyline').get('points'))
-    
-        return {'routes': polyline_string_list}
+            path_distance = 0
+            for leg in route['legs']:
+                path_distance = leg['distance']['value']
+            distance_array.append(path_distance)
+        return {'routes': polyline_string_list, 'distance_array': distance_array}
 
-    def fetchPathsInfoFromDB(self, route):
-	resultValue = None
-        m = hashlib.md5()
-        m.update(route.encode('utf-8'))
-        search_id =  m.hexdigest()
-        log.info("Path ID to be searched (md5):%s" %search_id)
-
-        c = a.findPathDetailsByID("mongodb://localhost","SAFE_ROUTE_DB","Paths_Info", str(search_id))
-        for j in c:
-	    crimeCount = j['crime_count']
-	    accidentCount = j['accident_count']
-	    policeCount = j['hospital_police_services']
-	    resultValue = self.weightedAverage(crimeCount, accidentCount, policeCount)
-
-        return resultValue
+    def fetchPathsInfoFromDB(self, all_route):
+        path_array = []
+        score_array = []
+        resultValue = 0
+        for route in all_route.get('routes'):
+            m = hashlib.md5()
+            m.update(route.encode('utf-8'))
+            search_id =  m.hexdigest()
+            log.info("Path ID to be searched (md5):%s" %search_id)
+            c = a.findPathDetailsByID("mongodb://localhost","SAFE_ROUTE_DB","Paths_Info", str(search_id))
+            for j in c:
+                crimeCount = j['crime_count']
+                accidentCount = j['accident_count']
+                policeCount = j['hospital_police_services']
+                resultValue = self.weightedAverage(crimeCount, accidentCount, policeCount)
+            path_array.append(search_id)
+            score_array.append(resultValue)
+        return {'score_array':score_array, 'path_array': path_array}
      
     def weightedAverage(self, crimeCount, accidentCount, policeCount):	
 	return ((crimeCount * CRIME_FACTOR_WEIGHTAGE) + (accidentCount * ACCIDENT_FACTOR_WEIGHTAGE) 
